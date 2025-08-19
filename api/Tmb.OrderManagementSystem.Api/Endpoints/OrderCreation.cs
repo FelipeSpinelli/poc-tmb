9b876f94679deefaeae20c3fc86ad2a7442bea8b
Felipe Spinelli <felipespinelli19@gmail.com>
2025-08-19 00:47:49 -0300

    :sparkles: creates order's creation endpoint

---
+++ api/Tmb.OrderManagementSystem.Api/Endpoints/OrderCreation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tmb.OrderManagementSystem.Core.Application.Models;
+using Tmb.OrderManagementSystem.Core.Application.Services.Abstractions;
+
+namespace Tmb.OrderManagementSystem.Api.Endpoints;
+
+public static class OrderCreation
+{
+    public class Request
+    {
+        public string Customer { get; init; } = null!;
+        public string Product { get; init; } = null!;
+        public decimal Price { get; init; }
+
+        public OrderCreationData AsOrderCreationData() => new()
+        { 
+            Customer = Customer,
+            Product = Product,
+            Price = Price
+        };
+    }
+
+    internal static void AddOrderCreation(this WebApplication app)
+    {
+        app.MapPost("/orders", async ([FromServices] IOrderService orderService, [FromBody] Request request, CancellationToken cancellationToken) =>
+        {
+            var orderCreationData = request.AsOrderCreationData();
+            var orderCreationResult = await orderService.CreateOrderAsync(orderCreationData, cancellationToken);
+
+            if (orderCreationResult.IsValid)
+            {
+                var id = orderCreationResult.Data!;
+                return (IResult)TypedResults.Created($"/orders/{id}", new { Id = id });
+            }
+
+            return (IResult)TypedResults.BadRequest(orderCreationResult.Errors);
+        })
+        .WithName("CreateOrder")
+        .WithOpenApi();
+    }
+}
--- api/Tmb.OrderManagementSystem.Api/Program.cs
+++ api/Tmb.OrderManagementSystem.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Tmb.OrderManagementSystem.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,11 +19,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", () =>
-{
-    return new object();
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.AddOrderCreation();
+
 
 app.Run();
